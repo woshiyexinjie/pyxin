3a63f366320a6c4daa4ca063ef17a661101e9c21
xjye <hellome335@gmail.com>
2019-08-02 15:26:00 +0800

    ADD Some python base and pydicom skill

--- utils/csvfile.py
+++ utils/csvfile.py
@@ -1,9 +1,42 @@
 
 import csv
+import codecs
 
-csvFile = open("instance.csv", "r")
 
-dict_reader = csv.DictReader(csvFile)
+#  csv的读取
+# csvFile = open("instance.csv", "r")
+#
+# dict_reader = csv.DictReader(csvFile)
+#
+# for row in dict_reader:
+#     print(row.get("TTT"))
 
-for row in dict_reader:
-    print(row.get("TTT"))
+
+# codecs 是自然语言编码转换模块
+
+fileName = 'PythonBook.csv'
+
+
+with codecs.open(fileName, 'w', 'utf-8') as csvfile:
+
+    filednames = ['书名', '作者']
+    writer = csv.DictWriter(csvfile, fieldnames=filednames)
+
+    books = []
+    book = {
+        'title': '笑傲江湖',
+        'author': '金庸',
+    }
+    book2 = {
+        'title': '西游记',
+        'author': '吴承恩',
+    }
+    books.append(book)
+    books.append(book2)
+
+    writer.writeheader()
+    for book in books:
+        try:
+            writer.writerow({'书名':book['title'], '作者':book['author']})
+        except UnicodeEncodeError:
+            print("编码错误, 该数据无法写到文件中, 直接忽略该数据")
